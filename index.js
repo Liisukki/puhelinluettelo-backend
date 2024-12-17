d62dbe93db37050d504a9dc4b1c899f74073d007
Liisu Kotilainen <91130860+Liisukki@users.noreply.github.com>
2024-12-17 16:11:42 +0200

    initial

---
+++ index.js
@@ -0,0 +1,85 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+
+app.use(cors());
+app.use(express.static('dist'))
+
+let persons = [
+  { name: "Arto Hellas", number: "040-123456", id: 1 },
+  { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
+  { name: "Dan Abramov", number: "12-43-234345", id: 3 },
+  { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
+  { name: "Arnold Schwartzenegger", number: "19-223-642121", id: 5 },
+];
+
+app.get("/", (request, response) => {
+  response.send("<h1>Hello World!</h1>");
+});
+
+app.get("/api/persons", (request, response) => {
+  response.json(persons);
+});
+
+app.get("/api/persons/:id", (request, response) => {
+  const id = Number(request.params.id);
+  const person = persons.find((person) => person.id === id);
+
+  if (person) {
+    response.json(person);
+  } else {
+    response.status(404).json({ error: "Person not found" });
+  }
+});
+
+app.delete("/api/persons/:id", (request, response) => {
+  const id = Number(request.params.id);
+  persons = persons.filter((person) => person.id !== id);
+  response.status(204).end();
+});
+
+const generateId = () => {
+  return Math.floor(Math.random() * 100000);
+};
+
+app.post("/api/persons", (request, response) => {
+  const body = request.body;
+
+  if (!body.name || !body.number) {
+    return response.status(400).json({
+      error: "Name or number is missing",
+    });
+  }
+
+  if (persons.find((person) => person.name === body.name)) {
+    return response.status(400).json({
+      error: "Name must be unique",
+    });
+  }
+
+  const newPerson = {
+    name: body.name,
+    number: body.number,
+    id: generateId(),
+  };
+
+  persons = persons.concat(newPerson);
+
+  response.json(newPerson);
+});
+
+app.get("/info", (request, response) => {
+  const numberOfPersons = persons.length;
+  const currentDate = new Date();
+
+  const info = `
+      <p>Phonebook has info for ${numberOfPersons} people</p>
+      <p>${currentDate}</p>
+    `;
+  response.send(info);
+});
+
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
