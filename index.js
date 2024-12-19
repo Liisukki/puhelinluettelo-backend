dea6b033233276e1f4dc5b4edb41dadf00f4c9cf
Liisu Kotilainen <91130860+Liisukki@users.noreply.github.com>
2024-12-19 21:14:37 +0200

    testing

--- index.js
+++ index.js
@@ -1,48 +1,52 @@
+require("dotenv").config();
 const express = require("express");
-const app = express();
 const cors = require("cors");
+const Person = require("./models/person");
+
+const app = express();
 
 app.use(cors());
-app.use(express.static('dist'))
+app.use(express.json());
+app.use(express.static("dist"));
 
-let persons = [
-  { name: "Arto Hellas", number: "040-123456", id: 1 },
-  { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
-  { name: "Dan Abramov", number: "12-43-234345", id: 3 },
-  { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
-  { name: "Arnold Schwartzenegger", number: "19-223-642121", id: 5 },
-];
-
+// Tervehdysreitti
 app.get("/", (request, response) => {
   response.send("<h1>Hello World!</h1>");
 });
 
-app.get("/api/persons", (request, response) => {
-  response.json(persons);
+// Näytä kaikki henkilöt tietokannasta
+app.get("/api/persons", (request, response, next) => {
+  Person.find({})
+    .then((persons) => {
+      response.json(persons);
+    })
+    .catch((error) => next(error));
 });
 
-app.get("/api/persons/:id", (request, response) => {
-  const id = Number(request.params.id);
-  const person = persons.find((person) => person.id === id);
-
-  if (person) {
-    response.json(person);
-  } else {
-    response.status(404).json({ error: "Person not found" });
-  }
+// Näytä yksittäinen henkilö ID:n perusteella
+app.get("/api/persons/:id", (request, response, next) => {
+  Person.findById(request.params.id)
+    .then((person) => {
+      if (person) {
+        response.json(person);
+      } else {
+        response.status(404).end();
+      }
+    })
+    .catch((error) => next(error));
 });
 
-app.delete("/api/persons/:id", (request, response) => {
-  const id = Number(request.params.id);
-  persons = persons.filter((person) => person.id !== id);
-  response.status(204).end();
+// Poista henkilö ID:n perusteella
+app.delete("/api/persons/:id", (request, response, next) => {
+  Person.findByIdAndRemove(request.params.id)
+    .then(() => {
+      response.status(204).end();
+    })
+    .catch((error) => next(error));
 });
 
-const generateId = () => {
-  return Math.floor(Math.random() * 100000);
-};
-
-app.post("/api/persons", (request, response) => {
+// Lisää uusi henkilö tietokantaan
+app.post("/api/persons", (request, response, next) => {
   const body = request.body;
 
   if (!body.name || !body.number) {
@@ -51,33 +55,46 @@ app.post("/api/persons", (request, response) => {
     });
   }
 
-  if (persons.find((person) => person.name === body.name)) {
-    return response.status(400).json({
-      error: "Name must be unique",
-    });
-  }
-
-  const newPerson = {
+  const person = new Person({
     name: body.name,
     number: body.number,
-    id: generateId(),
-  };
+  });
 
-  persons = persons.concat(newPerson);
-
-  response.json(newPerson);
+  person
+    .save()
+    .then((savedPerson) => {
+      response.json(savedPerson);
+    })
+    .catch((error) => next(error));
 });
 
-app.get("/info", (request, response) => {
-  const numberOfPersons = persons.length;
-  const currentDate = new Date();
+// Näytä tietokannan info
+app.get("/info", (request, response, next) => {
+  Person.find({})
+    .then((persons) => {
+      const numberOfPersons = persons.length;
+      const currentDate = new Date();
 
-  const info = `
-      <p>Phonebook has info for ${numberOfPersons} people</p>
-      <p>${currentDate}</p>
-    `;
-  response.send(info);
+      response.send(`
+        <p>Phonebook has info for ${numberOfPersons} people</p>
+        <p>${currentDate}</p>
+      `);
+    })
+    .catch((error) => next(error));
 });
+
+// Virheenkäsittelijä
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message);
+
+  if (error.name === "CastError" && error.kind === "ObjectId") {
+    return response.status(400).send({ error: "Malformatted ID" });
+  }
+
+  next(error);
+};
+
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
---
+++ readme.md
@@ -0,0 +1,1 @@
+https://puhelinluettelo-backend-0jql.onrender.com/
