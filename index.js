8fad046b0afa020bb60b742829e3ffba8eb3d729
Liisu Kotilainen <91130860+Liisukki@users.noreply.github.com>
2024-12-22 11:50:54 +0200

    3.22: lint-konfiguraatio

--- index.js
+++ index.js
@@ -1,133 +1,137 @@
-const express = require("express");
-const app = express();
-require("dotenv").config();
+const express = require('express')
+const app = express()
+require('dotenv').config()
 
-const Person = require("./models/person");
-app.use(express.static("dist"));
+const Person = require('./models/person')
+app.use(express.static('dist'))
 
 const requestLogger = (request, response, next) => {
-  console.log("Method:", request.method);
-  console.log("Path:  ", request.path);
-  console.log("Body:  ", request.body);
-  console.log("---");
-  next();
-};
+  console.log('Method:', request.method)
+  console.log('Path:  ', request.path)
+  console.log('Body:  ', request.body)
+  console.log('---')
+  next()
+}
 
+// Virhetilanteet
 const errorHandler = (error, request, response, next) => {
-  console.error(error.message);
+  console.error(error.message)
 
-  if (error.name === "CastError") {
-    return response.status(400).send({ error: "malformatted id" });
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
   }
 
-  if (error.name === "ValidationError") {
-    return response.status(400).json({ error: error.message });
+  if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
   }
 
-  next(error);
-};
+  next(error)
+}
 
-const cors = require("cors");
+const cors = require('cors')
 
-app.use(cors());
-app.use(express.json());
-app.use(requestLogger);
+app.use(cors())
+app.use(express.json())
+app.use(requestLogger)
 
 const unknownEndpoint = (request, response) => {
-  response.status(404).send({ error: "unknown endpoint" });
-};
+  response.status(404).send({ error: 'unknown endpoint' })
+}
 
-// POST route for creating a new person
-app.post("/api/persons", (request, response, next) => {
-  const body = request.body;
+// Uusi yhteystieto
+app.post('/api/persons', (request, response, next) => {
+  const body = request.body
 
   if (!body.name || !body.number) {
     return response.status(400).json({
-      error: "Name or number is missing",
-    });
+      error: 'Name or number is missing',
+    })
   }
 
   const person = new Person({
     name: body.name,
     number: body.number,
-  });
+  })
 
   person
     .save()
     .then((savedPerson) => {
-      response.json(savedPerson);
+      response.json(savedPerson)
     })
-    .catch((error) => next(error)); // Mongoosen validointivirheet käsitellään tässä
-});
+    .catch((error) => next(error))
+})
 
-// PUT route for updating an existing person
-app.put("/api/persons/:id", (request, response, next) => {
-  const { name, number } = request.body;
-  const id = request.params.id;
+// Yhteystiedon päivittäminen
+app.put('/api/persons/:id', (request, response, next) => {
+  const { name, number } = request.body
+  const id = request.params.id
 
-  const person = { name, number };
+  const person = { name, number }
 
   Person.findByIdAndUpdate(id, person, { new: true, runValidators: true })
     .then((updatedPerson) => {
       if (updatedPerson) {
-        response.json(updatedPerson);
+        response.json(updatedPerson)
       } else {
-        response.status(404).json({ error: "Person not found" });
+        response.status(404).json({ error: 'Person not found' })
       }
     })
-    .catch((error) => next(error)); // Virheiden käsittely
-});
+    .catch((error) => next(error)) // Virheiden käsittely
+})
 
-app.delete("/api/persons/:id", (request, response, next) => {
-  const id = request.params.id;
+app.delete('/api/persons/:id', (request, response, next) => {
+  const id = request.params.id
 
   Person.findByIdAndDelete(id)
     .then((result) => {
       if (result) {
-        response.status(204).end();
+        response.status(204).end()
       } else {
-        response.status(404).json({ error: "Person not found" });
+        response.status(404).json({ error: 'Person not found' })
       }
     })
-    .catch((error) => next(error)); // Virheiden käsittely
-});
+    .catch((error) => next(error)) // Virheiden käsittely
+})
 
-app.get("/api/persons", (request, response) => {
+app.get('/api/persons', (request, response) => {
   Person.find({}).then((persons) => {
-    response.json(persons);
-  });
-});
+    response.json(persons)
+  })
+})
 
-app.get("/api/persons/:id", (request, response, next) => {
-  const id = request.params.id;
+// Yksittäinen yhteystieto
+app.get('/api/persons/:id', (request, response, next) => {
+  const id = request.params.id
 
   Person.findById(id)
     .then((person) => {
       if (person) {
-        response.json(person);
+        response.json(person)
       } else {
-        response.status(404).json({ error: "Person not found" });
+        response.status(404).json({ error: 'Person not found' })
       }
     })
-    .catch((error) => next(error)); // Virheiden käsittely
-});
+    .catch((error) => next(error)) // Virheiden käsittely
+})
 
-app.get("/info", (request, response) => {
+// Montako yhteystietoa
+app.get('/info', (request, response) => {
   Person.countDocuments()
     .then((count) => {
-      const currentDate = new Date();
+      const currentDate = new Date()
       const info = `
         <p>Phonebook has info for ${count} people</p>
         <p>${currentDate}</p>
-      `;
-      response.send(info);
+      `
+      response.send(info)
     })
-    .catch((error) => next(error)); // Virheiden käsittely
-});
+    .catch((error) => next(error)) // Virheiden käsittely
+})
 
-app.use(errorHandler);
+app.use(unknownEndpoint)
+app.use(errorHandler)
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+  console.log(`Server running on port ${PORT}`)
+})
